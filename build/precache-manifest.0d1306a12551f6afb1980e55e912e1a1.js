fe35cdeba1fffc88a4daeeaf44a7971aa72b7942
Nicolas KREMER <nico.kremer@gmail.com>
2019-01-28 09:50:14 +0100

    Created build

---
+++ build/precache-manifest.0d1306a12551f6afb1980e55e912e1a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48fd8badcc2c33aaf661",
+    "url": "/static/css/main.26deca9f.chunk.css"
+  },
+  {
+    "revision": "48fd8badcc2c33aaf661",
+    "url": "/static/js/main.48fd8bad.chunk.js"
+  },
+  {
+    "revision": "86b9db1454b2859bc989",
+    "url": "/static/js/1.86b9db14.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "30c47b196963f279885197e789d2e7a2",
+    "url": "/index.html"
+  }
+];
