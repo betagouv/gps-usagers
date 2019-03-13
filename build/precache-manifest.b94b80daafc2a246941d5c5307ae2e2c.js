12e757b063c67d1d2c3c26faa92c810b59e8f4b7
soulso <nico.kremer@gmail.com>
2019-03-13 22:15:05 +0100

    fixed modal

---
+++ build/precache-manifest.b94b80daafc2a246941d5c5307ae2e2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c177035cb353ad5facd1",
+    "url": "/static/css/main.f6674641.chunk.css"
+  },
+  {
+    "revision": "c177035cb353ad5facd1",
+    "url": "/static/js/main.c177035c.chunk.js"
+  },
+  {
+    "revision": "90fe335dfd8defffa4e9",
+    "url": "/static/js/1.90fe335d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3299245b5442fb6a13ff0d45d19c29cd",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71f75a9f5584627ce2738d89162ec0cc.js"
+  "/precache-manifest.b94b80daafc2a246941d5c5307ae2e2c.js"
 );
 
 workbox.clientsClaim();
