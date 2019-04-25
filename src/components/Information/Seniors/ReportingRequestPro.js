b3af76253049a1a15997ff387ea48584f8644f24
soulso <nico.kremer@gmail.com>
2019-04-25 15:49:52 +0200

    Updated final 16$

--- src/components/Information/Seniors/ReportingRequestPro.js
+++ src/components/Information/Seniors/ReportingRequestPro.js
@@ -24,7 +24,13 @@ const InfoSeniorReportingRequestPro = ({ transition, machineState }) => {
       </div>
       <div className="content final">
         <h1>Vous souhaitez signaler un événement indésirable :</h1>
-        Merci de répondre au formulaire ci-dessous
+        Merci de répondre au formulaire ci-dessous <br />
+        <a
+          href="https://www.calvados.fr/files/live/sites/calvados/files/documents/Le%20D%C3%A9partement/Solidarit%C3%A9%20%26%20famille/Autonomie%20%26%20Handicap/Formulaire-de-signalement-a-destination-du-Departement-du-Calvados.pdf"
+          target="_top"
+        >
+          https://www.calvados.fr/files/live/sites/calvados/files/documents/Le%20D%C3%A9partement/Solidarit%C3%A9%20%26%20famille/Autonomie%20%26%20Handicap/Formulaire-de-signalement-a-destination-du-Departement-du-Calvados.pdf
+        </a>
         <p />
         <br />
         <p>
