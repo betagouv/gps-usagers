0a5479a961bab42b46c6b04d111fd3e09b4fe7c7
soulso <nico.kremer@gmail.com>
2019-05-07 14:19:24 +0200

    fix(Handicapped): Fixed CAF screen

--- src/components/Information/Handicapped/PaymentCaf.js
+++ src/components/Information/Handicapped/PaymentCaf.js
@@ -24,32 +24,17 @@ const InfoHandicappedPchCaf = ({ transition, machineState }) => {
       </div>
       <div className="content final">
         <h1>Contactez la Caisse d'Allocations Familiales (CAF)</h1>
-        Rendez-vous sur votre compte CAF :
+        Rendez-vous sur votre CAF :
         <p>
-          <button
-            className="button"
-            onClick={() =>
-              window.open(
-                "https://wwwd.caf.fr/wps/portal/caffr/login/!ut/p/a1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOID_A3dPbyDDdz9A1yNDTxdzQNDXJ19DS0CjYAKIoEKDHAARwNC-sP1o8BK8JjgpR-VnpOfBHZNpGNekrFFun5UUWpaalFqkV5pEVA4o6SkwErVQNWgvLxcLzkxTS-tSNUAm-qM_OIS_QiEKv2C3AgD3aikynJHRUUAO_ejAA!!/dl5/d5/L2dBISEvZ0FBIS9nQSEh/#/signature",
-                "_blank"
-              )
-            }
-          >
-            Accèder à votre compte
-          </button>
-        </p>
-        <br />
-        <p>
-          Si vous avez oublié votre numéro d'allocataire ou votre mot de passe,
-          suivez les instructions sur{" "}
           <a
-            href="http://www.caf.fr/allocataires/aide/utiliser-mon-compte"
+            href="http://www.caf.fr/allocataires/droits-et-prestations/connaitre-vos-droits-selon-votre-situation/je-suis-dans-une-situation-de-handicap"
             rel="noopener noreferrer"
             target="_blank"
           >
-            cette page
+            http://www.caf.fr/allocataires/droits-et-prestations/connaitre-vos-droits-selon-votre-situation/je-suis-dans-une-situation-de-handicap
           </a>
         </p>
+        <br />
         <p>
           Pour joindre la CAF par téléphone, appelez le numéro suivant :
           <h2 className="phone">
